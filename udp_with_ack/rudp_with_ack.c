e5f70b92f87af3335a0df235683c46830f50fced
RyuseiShiiba <33246305+shiibaryu@users.noreply.github.com>
2018-12-15 23:54:17 +0900

    Update rudp_with_ack.c

--- udp_with_ack/rudp_with_ack.c
+++ udp_with_ack/rudp_with_ack.c
@@ -17,6 +17,12 @@
 
 typedef enum{SYN_SENT = 0,OPENING,OPEN,FIN_SENT}rudp_state_t;
 
+struct data {
+    void *item;
+    int len;
+    struct data *next;
+}
+
 struct rudp_hdr{
     u_int16_t version;
     u_int16_t type;
@@ -65,7 +71,7 @@ bool_t rng_seeded = false;
 struct rudp_socket_list *socket_list_head = NULL;
 
 
-void create_receiver_session(struct sockaddr_in *addr,u_int32_t seqno,struct rudp_socket_list *socket){
+void create_sender_session(struct rudp_socket_list *socket, u_int32_t seqno, struct sockaddr_in *to, struct data **data_queue) {
     struct session *new_session = malloc(sizeof(struct session));
     if(new_session = NULL){
         fprintf(stderr,"create_receive_session: Error allocating memory\n");
@@ -117,7 +123,78 @@ struct rudp_packet *create_rudp_packet(u_int16_t type,u_int32_t seqno,int len,ch
     return packet;
 }
 
-int send_packet(struct rudp_packet *p,struct sockaddr_in *recipient,rudp_socket_t rsocket,){
+rudp_socket_t rudp_socket(int port){
+    if(rng_seeded == false){
+        srand(time(NULL));
+        rng_seeded = true;
+    }
+    int sockfd;
+    struct sockaddr_in address;
+
+    socketfd = socket(AF_INET,SOCK_DGRAM,0);
+    if(sockfd < 0){
+        perror("socket");
+        return(rudp_socket_t)NULL;
+    }
+
+    memset(&address,0,sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if(bind(sockfd,(struct *)&address,sizeof(address))<0){
+        perror("bind");
+        return NULL;
+    }
+
+    rudp_socket_t socket = (rudp_socket_t)socketfd;
+
+    struct rudp_socket_list *newsocket = malloc(sizeof(rudp_socket_list));
+    if(new_socket == NULL){
+        fprintf(stderr,"rudp_socket: Error allocating memory for socket list");
+        return (rudp_socket_t)-1;
+    }
+
+    newsocket->rsock = socket;
+    newsocket->close_requested = false;
+    newsocket->sessions_list_head = NULL;
+    newsocket->next = NULL;
+    newsocket->handler = NULL;
+    newsocket->recv_handler = NULL;
+
+    if(socket_list_head == NULL){
+        socket_list_head = new_socket;
+    }
+    else{
+        struct rudp_socket_list *curr = socket_list_head;
+        while(curr->next != NULL){
+            curr = curr->next;
+        }
+        curr->next = new_socket;
+    }
+    if(event_fd(socketfd,receive_callback,(void*)socketfd,"rudp_callback")<0){
+        fprintf(stderr,"Error registering receive callback function");
+    }
+
+    return socket;
+}
+
+int rudp_close(rudp_socket_t rsocket){
+    struct rudp_socket_list *curr_socket = socket_list_head;
+    while(curr_socket->next != NULL){
+        if(curr_socket->rsock == rsocket){
+            break;
+        }
+        curr_socket = curr_socket->next;
+    }
+    if(curr_socket->rsock == rsocket){
+        curr_socket->close_requested = true;
+        return 0;
+    }
+    return -1;
+}
+
+int send_packet(bool_t is_ack, rudp_socket_t rsocket, struct rudp_packet *p, struct sockaddr_in *recipient) {
     char type[5];
     short t = p -> header.type;
     if(t == 1){
@@ -148,7 +225,6 @@ int send_packet(struct rudp_packet *p,struct sockaddr_in *recipient,rudp_socket_
     }
 
     if(!isack){
-    /*Set a timeout event if the packet isn't an ACK*/
     struct timeoutargs *timeargs = malloc(sizeof(struct timeoutargs));
     if(timeags == NULL){
         fprintf(stderr,"send_packet:Error allocating timeout args\n");
@@ -160,10 +236,111 @@ int send_packet(struct rudp_packet *p,struct sockaddr_in *recipient,rudp_socket_
         return -1;
     }
     timeargs->fd = rsocket;
-    memcpy(timeargs)
+    memcpy(timeargs->packet,p.sizeof(struct rudp_packet));
+    memcpy(timeargs->recipient,recipient,sizeof(struct sockaddr));
+
+    struct timeval currentTime;
+    gettimeofday(&currentTime,NULL);
+    struct timeval delay;
+    delay.tv_sec = RUDP_TIMEOUT/1000;
+    delay.tv_sec = 0;
+    struct timeval timeout_time;
+    timeradd(&currentTime,&delay,&timeout_time);
+
+    struct rudp_socket_list *curr_socket = socket_list_head;
+    while(curr_socket != NULL){
+        if(curr_scoket->rsock == timeargs->fd){
+            break;
+        }
+        curr_socket = curr_socket->next;
+    }
+    if(curr_socket->rsock == timeargs->fd){
+      bool_t session_found = false;
+      struct session *curr_session = curr_socket->session_list_head;
+      while(curr_session != NULL){
+          if(compare_sockaddr(&curr_session->address,timeargs->recipient)==1){
+              session_found = true;
+              break;
+          }
+          curr_session = curr_session->next;
+      }
+      if(session_found){
+          if(timeargs->packet->header.type == RUDP_SYN){
+              curr_session->sender->syn_timeout_arg = timeargs;
+          }
+          else if(timeargs->packet->header.type == RUDP_FIN){
+              curr_session->sender->sym_timeout_arg = timeargs;
+          }
+          else if(timeargs->packet->header.type == RUDP_DATA){
+              int i;
+              int index;
+              for(i = 0;i<RUDP_WINDOW;i++){
+                  if(curr_session->sender->sliding_window[i] != NULL && curr_session->sender->sliding_window[i]->header.seqno == timeargs->packet->header.seqno){
+                      index = i;
+                  }
+              }
+              curr_session->sender->data_timeout_arg[index] = timeargs;
+        }
+      }
+    }
+    event_timeout(timeout_time, timeout_callback, timeargs, "timeout_callback");
+   }
+   return 0;
+}
+
+int rudo_sendto(rudp_socket_t rsock,void *data,int len,struct sockaddr_in *to){
+    if(len < 0 || len > RUDP_MAXPKTSIZE){
+        fprintf(stderr,"rudp_sendto Error: attempting to send with invalid packet size\n");
+        return -1;
+    }
+    if(rsock < 0){
+        fprintf(stderr,"rudp_sendto Error: attempting to send with rudp_socket"):
+        return -1;
+    }
+    if(to == NULL){
+        fprintf(stderr,"rudp_sendto Error: attempting to send an invalid address\n");
+        return -1;
     }
 
+    bool_t new_session_created = true;
+    u_int32_t seqno = 0;
+    if(socket_list_head == NULL){
+        fprintf(stderr,"Error: attempt to send on invalid socket.No socket in the list.");
+        return -1;
+        }
+    else{
+        struct rudp_socket_list *curr = session_list_head;
+        while(curr != NULL){
+            if(curr->rsock == rsock){
+                break;
+            }
+            curr = curr->next;
+        }
+        if(curr->rsock == rsock){
+            struct data *data_item = malloc(sizeof(struct data));
+            if(data_item == NULL){
+                fprintf(stderr,"rudp_sendto: Error allocating data queue\n");
+                return -1;
+            }
+            data_item->item = malloc(len);
+            if(data_item->item == NULL){
+                fprintf(stderr,"rudp_sendto: Error allocating data queue item");
+                return -1;
+            }
+            memcpy(data_item->item,data,len);
+            data_item->len = len;
+            data_item->next = NULL;
 
+            if(curr -> session_list_head == NULL){
+                seqno = rand();
+                /*void create_sender_session(struct rudp_socket_list *socket, u_int32_t seqno, struct sockaddr_in *to, struct data **data_queue) {*/
+                create_sender_sesssion(curr,seqno,to,&data_item);
+            }
+            
+        }
+
+    }
+    
 }
 
 int receive_callback(int file,void *arg){
@@ -203,7 +380,7 @@ int receive_callback(int file,void *arg){
     inet_ntoa(sender.sin_addr),ntohs(sender.sin_port),rudpheader.seqno,file);
 
     if(socket_list_head == NULL){
-        fprintf(stderr,"Error: attempt to receive on invalid socket.No sockets in the list.\n");
+        fprintf(stderr,"Error: attempt to receive on invalid socket. No sockets in the list.\n");
         return -1;
     }
     else{
@@ -228,8 +405,40 @@ int receive_callback(int file,void *arg){
                  /* No sessions exist and we got a non-SYN, so ignore it */   
                 }
             }
-
-
+            else{
+                /*他のセッションあった場合*/
+                bool_t session_found = false;
+                struct session *curr_session = curr_socket -> session_list_head;
+                struct session *last_session;
+                while(curr_session != NULL){
+                    if(curr_session->next == NULL){
+                        last_session = curr_session;
+                    }
+                    if(compare_sockaddress(&curr_session->address,&sender) == 1){
+                        session_found = true;
+                        break;
+                    }
+                    curr_session = curr_session->next;
+                }
+                if(session_found == false){
+                    if(rudpheader.type == RUDP_ACK){
+                        u_int32_t seqno = rudptype.seqno + 1;
+                        create_receiver_session(curr_socket,seqno,&sender);
+                        struct rudp_packet *p  = create_rudp_packet(RUDP_ACK,seqno,0,NULL);
+                        send_packet(true,(rudp_sock_t) file,p,&sender);
+                        free(p);
+                    }
+                    else{
+                    }
+                }
+                else{
+                    if(rudpheader.type = RUDP_SYN){
+                        if(curr_session->receiver == NULL || curr_session ->receiver->status = OPENING ){}
+                    }
+                }
+            }
         }
     }
 }
+
+
